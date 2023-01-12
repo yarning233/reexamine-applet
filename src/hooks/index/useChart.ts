636c8ac83816e47da1a81f9daec0ee5a742a3fc8
yarning233 <kerui_wu@163.com>
2023-01-12 11:52:33 +0800

    refactor: 封装首页hooks

--- src/hooks/index/useChart.ts
+++ src/hooks/index/useChart.ts
@@ -1,5 +1,9 @@
 // @ts-ignore
 import { ref } from 'vue'
+import { queryAdjustList, scoreOverTheYears } from "../../api/adjust"
+import { years } from "../useYears"
+import { state } from "./useState"
+import { ChartQueryType } from "../../types/adjust"
 
 const chartRef = ref()
 const majorChartRef = ref()
@@ -23,17 +27,154 @@ const scoreLineYearData = ref([])
 const scoreLineValueData = ref([])
 const scoreOption = ref()
 
+const queryAdjustChartData = async () => {
+	const res = await queryAdjustList({
+		year: years.value[state.tab11value],
+		category: parseInt(state.pieChartType) as (0 | 1)
+	} as ChartQueryType)
+
+	if (res.code === 200) {
+		if (state.pieChartType === '0') {
+			if (res.data.length !== 0) {
+				const { count, nineHundred, twoEleven, initiative, selfLineation } = res.data[0]
+
+				collegeCount.value = count
+				collegeData.value = [
+					{
+						name: '985',
+						value: parseInt(nineHundred)
+					},
+					{
+						name: '211',
+						value: parseInt(twoEleven)
+					},
+					{
+						name: '双一流',
+						value: parseInt(initiative)
+					},
+					{
+						name: '自划线',
+						value: parseInt(selfLineation)
+					}
+				]
+
+				collegeOption.value = {
+					title: {
+						text: `全国一共有 ${ collegeCount.value ? collegeCount.value : 0 } + 所院校参与复试`,
+						left: 'center'
+					},
+					tooltip: {
+						trigger: 'item'
+					},
+					color: [
+						'#158AD2',
+						'#69BFFF',
+						'#50ADA3',
+						'#00DEAE'
+					],
+					series: [
+						{
+							name: '院校图表',
+							type: 'pie',
+							radius: '50%',
+							data: collegeData.value,
+							emphasis: {
+								itemStyle: {
+									shadowBlur: 10,
+									shadowOffsetX: 0,
+									shadowColor: 'rgba(0, 0, 0, 0.5)'
+								}
+							}
+						}
+					]
+				}
+
+				chartRef.value.init(collegeOption.value)
+			}
+		} else {
+			majorData.value =  res.data.map(item => {
+				return {name: item.categoryName, value: item.majorcount}
+			})
+			res.data.map((item) => {
+				majorDataCount.value += item.majorcount
+			})
+
+			majorOption.value = {
+				title: {
+					text: `全国一共有 ${ majorDataCount.value ? majorDataCount.value : 0  } + 专业参与复试`,
+					left: 'center'
+				},
+				tooltip: {
+					trigger: 'item'
+				},
+				color: [
+					'#158AD2',
+					'#E34890',
+					'#37C190',
+					'#00D9DB',
+					'#158AD2',
+					'#A94BA0',
+					'#C8628C',
+					'#00DEAE'
+				],
+				series: [
+					{
+						name: '专业图表',
+						type: 'pie',
+						radius: '50%',
+						data: majorData,
+						emphasis: {
+							itemStyle: {
+								shadowBlur: 10,
+								shadowOffsetX: 0,
+								shadowColor: 'rgba(0, 0, 0, 0.5)'
+							}
+						}
+					}
+				]
+			}
+
+			majorChartRef.value.init(majorOption.value)
+		}
+	}
+}
+
+const queryScoreLine = async () => {
+	const res = await scoreOverTheYears()
+
+	if (res.code === 200) {
+		scoreLineYearData.value = res.data.map(item => item.particularYear)
+		scoreLineValueData.value = res.data.map(item => item.fractionalLine)
+
+		scoreOption.value = {
+			xAxis: {
+				type: 'category',
+				boundaryGap: false,
+				data: scoreLineYearData
+			},
+			yAxis: {
+				type: 'value'
+			},
+			color: 'darkcyan',
+			series: [
+				{
+					data: scoreLineValueData,
+					type: 'line'
+				}
+			]
+		}
+
+		scoreChartRef.value.init(scoreOption.value)
+	}
+}
+
 export {
 	chartRef,
 	majorChartRef,
 	scoreChartRef,
 	collegeOption,
-	collegeCount,
-	collegeData,
 	majorOption,
-	majorDataCount,
-	majorData,
 	scoreOption,
-	scoreLineYearData,
-	scoreLineValueData
+	queryAdjustChartData,
+	queryScoreLine
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,8 +3,6 @@ import { reactive, ref, watch, onMounted } from "vue"
 import Taro from '@tarojs/taro'
 import styles from './index.module.scss'
 import DataChart from "../../components/data-chart"
-import { ChartQueryType } from '../../types/adjust/index'
-import {queryAdjustList, scoreOverTheYears} from '../../api/adjust'
 import ECanvas from '../../components/ec-canvas/index'
 import judge from './../../hooks/useJudge'
 import { state, authState, getUserProfile, getPhoneNumber } from '../../hooks/index/useState'
@@ -21,14 +19,10 @@ import {
 	majorChartRef,
 	scoreChartRef,
 	collegeOption,
-	collegeCount,
-	collegeData,
 	majorOption,
-	majorDataCount,
-	majorData,
 	scoreOption,
-	scoreLineYearData,
-	scoreLineValueData
+	queryAdjustChartData,
+	queryScoreLine
 } from '../../hooks/index/useChart'
 
 export default {
@@ -39,147 +33,6 @@ export default {
 			state.currentYear = years.value[state.tab11value]
 			queryAdjustChartData()
 		})
-
-		const queryAdjustChartData = async () => {
-			const res = await queryAdjustList({
-				year: years.value[state.tab11value],
-				category: parseInt(state.pieChartType) as (0 | 1)
-			} as ChartQueryType)
-
-			if (res.code === 200) {
-				if (state.pieChartType === '0') {
-					if (res.data.length !== 0) {
-						const { count, nineHundred, twoEleven, initiative, selfLineation } = res.data[0]
-
-						collegeCount.value = count
-						collegeData.value = [
-							{
-								name: '985',
-								value: parseInt(nineHundred)
-							},
-							{
-								name: '211',
-								value: parseInt(twoEleven)
-							},
-							{
-								name: '双一流',
-								value: parseInt(initiative)
-							},
-							{
-								name: '自划线',
-								value: parseInt(selfLineation)
-							}
-						]
-
-						collegeOption.value = {
-							title: {
-								text: `全国一共有 ${ collegeCount.value ? collegeCount.value : 0 } + 所院校参与复试`,
-								left: 'center'
-							},
-							tooltip: {
-								trigger: 'item'
-							},
-							color: [
-								'#158AD2',
-								'#69BFFF',
-								'#50ADA3',
-								'#00DEAE'
-							],
-							series: [
-								{
-									name: '院校图表',
-									type: 'pie',
-									radius: '50%',
-									data: collegeData.value,
-									emphasis: {
-										itemStyle: {
-											shadowBlur: 10,
-											shadowOffsetX: 0,
-											shadowColor: 'rgba(0, 0, 0, 0.5)'
-										}
-									}
-								}
-							]
-						}
-
-						chartRef.value.init(collegeOption.value)
-					}
-				} else {
-					majorData.value =  res.data.map(item => {
-						return {name: item.categoryName, value: item.majorcount}
-					})
-					res.data.map((item) => {
-						majorDataCount.value += item.majorcount
-					})
-
-					majorOption.value = {
-						title: {
-							text: `全国一共有 ${ majorDataCount ? majorDataCount : 0  } + 专业参与复试`,
-							left: 'center'
-						},
-						tooltip: {
-							trigger: 'item'
-						},
-						color: [
-							'#158AD2',
-							'#E34890',
-							'#37C190',
-							'#00D9DB',
-							'#158AD2',
-							'#A94BA0',
-							'#C8628C',
-							'#00DEAE'
-						],
-						series: [
-							{
-								name: '专业图表',
-								type: 'pie',
-								radius: '50%',
-								data: majorData,
-								emphasis: {
-									itemStyle: {
-										shadowBlur: 10,
-										shadowOffsetX: 0,
-										shadowColor: 'rgba(0, 0, 0, 0.5)'
-									}
-								}
-							}
-						]
-					}
-
-					majorChartRef.value.init(majorOption.value)
-				}
-			}
-		}
-
-		const queryScoreLine = async () => {
-			const res = await scoreOverTheYears()
-
-			if (res.code === 200) {
-				scoreLineYearData.value = res.data.map(item => item.particularYear)
-				scoreLineValueData.value = res.data.map(item => item.fractionalLine)
-
-				scoreOption.value = {
-					xAxis: {
-						type: 'category',
-						boundaryGap: false,
-						data: scoreLineYearData
-					},
-					yAxis: {
-						type: 'value'
-					},
-					color: 'darkcyan',
-					series: [
-						{
-							data: scoreLineValueData,
-							type: 'line'
-						}
-					]
-				}
-
-				scoreChartRef.value.init(scoreOption.value)
-			}
-		}
 
 		onMounted(() => {
 			Taro.login({

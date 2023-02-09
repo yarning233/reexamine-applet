76ad839ec10a1d62d220ed348380faaa1bc68e4a
yarning233 <kerui_wu@163.com>
2023-02-09 17:16:12 +0800

    feat: 修改广告图及跳转链接&修改默认年份、优先显示专业

--- src/pages/advance/index.tsx
+++ src/pages/advance/index.tsx
@@ -1,9 +1,35 @@
-import { defineComponent } from 'vue'
+import Taro from '@tarojs/taro'
+import { defineComponent, onMounted } from 'vue'
 
 const Advance = defineComponent({
 	setup() {
+		Taro.showToast({
+			title: '加载中',
+			icon: 'loading',
+			duration: 10000
+		})
+
+		onMounted(() => {
+			console.log(1)
+			Taro.downloadFile({
+				url: 'https://kaoyancun.oss-cn-hangzhou.aliyuncs.com/img/fsbd.pdf',
+				success: function (res) {
+					const filePath = res.tempFilePath
+		
+					Taro.openDocument({
+						filePath,
+						success(res) {
+							console.log(res)
+							Taro.navigateBack()
+						}
+					})
+				}
+			})
+		})
+
 		return () => (
-			<web-view src="https://www.kaoyan1v1.com/vip/fushi-3"></web-view>
+			<div></div>
+			// <web-view src="https://www.kaoyan1v1.com/fsbd"></web-view>
 		)
 	}
 })

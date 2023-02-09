3314aa41d72af7f0fc2a9a40896294a93633cab6
yarning233 <kerui_wu@163.com>
2023-02-09 17:23:43 +0800

    fix: 开屏广告点击跳转PDF预览页、倒计时跳转首页

--- src/pages/initiate/index.tsx
+++ src/pages/initiate/index.tsx
@@ -8,6 +8,12 @@ const Initiate = defineComponent({
 		const second = ref<number>(5)
 
 		const goIndex = () => {
+			Taro.switchTab({
+				url: '/pages/index/index'
+			})
+		}
+
+		const goAdvancePage= () => {
 			Taro.navigateTo({
 				url: '/pages/advance/index'
 			})
@@ -25,7 +31,7 @@ const Initiate = defineComponent({
 		})
 
 		return () => (
-			<view class={ styles.contain }>
+			<view class={ styles.contain } onClick={ goAdvancePage }>
 				<view class={ styles.close } onClick={ goIndex }>
 					{ second.value }S
 					<nut-icon name="close" size={'12px'}></nut-icon>
